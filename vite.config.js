ec831ff2513ccce09fbee00eb70b132985f7469e
rtondev <rton.dev@gmail.com>
2025-02-25 08:54:25 -0300

    udpate env

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,18 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_API_URL || 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  }
 })
